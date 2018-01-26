b27353bebdd5ee0b3add3d825f21753eb3d4f73e
ViljaKettu <viljakettu@gmail.com>
2018-01-26 21:55:25 +0200

    Random Message
    alustava viestin randomisointi koodi

--- Transmission/Assets/Scripts/RndMessages.cs
+++ Transmission/Assets/Scripts/RndMessages.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RndMessages : MonoBehaviour {
+public class RndMessages : MonoBehaviour
+{
+    string[] texts = new string[] {"text1 ", "text3 ", "text3 " };
+    string[] texts2 = new string[] { "laa ", "lalla ", "lala " };
+    string[] texts3 = new string[] { "kissa", "koira", "marsu" };
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    string currentText;
+    string currentText2;
+    string currentText3;
+
+    public Text message;
+
+    private void Start()
+    {
+
+        currentText = texts[Random.Range(0, texts.Length)];
+        currentText2 = texts2[Random.Range(0, texts.Length)];
+        currentText3 = texts3[Random.Range(0, texts.Length)];
+
+        message.text = currentText + currentText2 + currentText3;
+    }
 }
