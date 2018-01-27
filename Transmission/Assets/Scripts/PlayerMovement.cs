c6b433d7257a08c0e4ba556ec9163d61ad168b94
ViljaKettu <viljakettu@gmail.com>
2018-01-27 14:31:39 +0200

    FootstepsSounds

--- Transmission/Assets/Scripts/PlayerMovement.cs
+++ Transmission/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    AudioClip footsteps;
+    AudioSource audio;
 
     public float speed, hor, ver;
     public bool canMove;
@@ -12,16 +14,34 @@ public class PlayerMovement : MonoBehaviour
     void Start ()
     {
         rb = GetComponent<Rigidbody2D>();
-	}
+        audio = GetComponent<AudioSource>();
+    }
 	
 	void Update ()
     {
-        if(canMove)
+
+        PlaySound();
+        if (canMove)
         {
             hor = Input.GetAxisRaw("Horizontal");
             ver = Input.GetAxisRaw("Vertical");
-
             rb.MovePosition(rb.position + new Vector2(hor, ver) * speed * Time.deltaTime);
         }
-	}
+    }
+
+    void PlaySound()
+    {
+        if(!audio.isPlaying)
+        {
+            if (Mathf.Abs(hor) > 0 || Mathf.Abs(ver) > 0)
+            {
+                audio.Play();
+            }
+        }
+        else if (hor == 0 && ver == 0)
+        {
+            audio.Stop();
+        }
+
+    }
 }
